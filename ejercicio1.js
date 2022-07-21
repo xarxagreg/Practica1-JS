6d21d6a68487cffb0323f4bb45ba1b2b97868d49
xarxagreg <106590042+xarxagreg@users.noreply.github.com>
2022-07-21 19:52:00 +0200

    Add files via upload

--- ejercicio1.js
+++ ejercicio1.js
@@ -2,14 +2,17 @@
 let numero1=parseInt(prompt("Introduzca el número A"));
 let numero2=parseInt(prompt("Introduzca el número B"));
 
+console.log(`El número "A" introducido tiene un valor de: ${numero1} el número "B" introducido tiene un valor de: ${numero2} `)
+
+
 if (numero1>numero2){
-    console.log(`El número mayor es el número A con un valor de: ${numero1}`)
+    console.log(`El número mayor es el número "A" con un valor de: ${numero1}`)
 }
 else if(numero2>numero1){
-    console.log(`El número mayor es el número B con un valor de: ${numero2}`)
+    console.log(`El número mayor es el número "B" con un valor de: ${numero2}`)
 
 }
 else
-console.log(`Lo sentimos el número A y B son iguales, debe elegir números diferentes`)
+console.log(`Lo sentimos el número "A y B" son iguales, debe elegir números diferentes`)
 
 
