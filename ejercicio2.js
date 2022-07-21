63f85a7c85769c769f91e8b60895ccc2d3176868
xarxagreg <106590042+xarxagreg@users.noreply.github.com>
2022-07-21 12:41:29 +0200

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,16 @@
+
+let numero1=parseInt(prompt("Introduzca el número A"));
+let numero2=parseInt(prompt("Introduzca el número B"));
+
+
+while( numero1==numero2){
+    numero2=parseInt(prompt("Introduzca el número B"))}
+
+if (numero1>numero2){
+    console.log(`El número mayor es el número A con un valor de: ${numero1}`)
+}
+else 
+    
+    console.log(`El número mayor es el número B con un valor de: ${numero2}`)
+
+
